b254e655703ad73a35ad04f079ee510e1ed5d84b
Sebastian Olsson <sebas-o@online.no>
2024-04-25 09:36:53 +0200

    fixed calculator

--- Kalkulator/JS/script.js
+++ Kalkulator/JS/script.js
@@ -1,5 +1,37 @@
-const display = document.getElementById('display')
+const display = document.getElementById('display');
 
-const addToDisplay = (value) => display.value += value
-const clearDisplay = () => display.value = ''
-const calc = () => display.value = eval(display.value)
+const symbols = ['-', '+', '/', '*'];
+
+const addToDisplay = (value) => {
+    const currentValue = display.value;
+    const lastChar = currentValue[currentValue.length - 1];
+    const secondLastChar = currentValue[currentValue.length - 2];
+
+    if (!symbols.includes(value)) {
+        display.value += value
+        return
+    }
+    if (currentValue === '') {
+        if (value != '-') return
+        else { 
+            display.value += value
+            return
+        }
+    }
+    else if (value == '*' && symbols.includes(lastChar)) {
+        if (lastChar == '*' && secondLastChar == '*') return
+        else if (lastChar != '*') return
+        display.value += value
+        return
+    }
+    else if (symbols.includes(lastChar) && value !== '*') {
+        return
+    } 
+    else {
+        display.value += value;
+        return
+    }
+};
+
+const clearDisplay = () => display.value = '';
+const calc = () => display.value = eval(display.value);
